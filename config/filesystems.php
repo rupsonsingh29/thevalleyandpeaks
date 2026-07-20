ee5de6978334f684fe409a2e984ffc57dea4a924
Rushma Singh <rushmasingh017@gmail.com>
2026-07-20 17:52:31 +0545

    point public disk to external storage outside deploy path

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,7 +40,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => '/home/u807825412/app-storage/public',
             'url' => rtrim(env('APP_URL', 'http://localhost'), '/').'/storage',
             'visibility' => 'public',
             'throw' => false,
